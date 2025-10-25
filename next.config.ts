fcb3f013b3fb1a48387c918fd958e4ad77099186
Shema Patrick <shemapatrick06@gmail.com>
2025-10-25 02:44:13 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -23,9 +23,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-      },
-      {
         protocol: 'https',
         hostname: 'i.pinimg.com',
         port: '',
